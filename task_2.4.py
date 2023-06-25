2d6c459ebfb77960da56bfcabbd68e87b81175be
ain1962 <134201258+ain1962@users.noreply.github.com>
2023-06-25 20:43:04 +0700

    task_2.4
    Решение задачи 2.4 п. А

---
+++ task_2.4.py
@@ -0,0 +1,43 @@
+# Задача 2.4.
+
+# Пункт A.
+# Напишите функцию, которая удаляет все восклицательные знаки из заданной строк.
+# Например,
+# foo("Hi! Hello!") -> "Hi Hello"
+# foo("") -> ""
+# foo("Oh, no!!!") -> "Oh, no"
+
+
+foo='"Hi! Hello!", "", "Oh, no!!!"'
+s=foo.replace('!','')
+def remove_exclamation_marks(s):
+
+    return(s)
+
+print(remove_exclamation_marks(s))
+
+
+# Пункт B.
+# Удалите восклицательный знак из конца строки. 
+# remove("Hi!") == "Hi"
+# remove("Hi!!!") == "Hi!!"
+# remove("!Hi") == "!Hi"
+
+
+
+# Дополнительно
+
+# Пункт С.
+# Удалите слова из предложения, если они содержат ровно один восклицательный знак.
+# Слова разделены одним пробелом.
+# Например,
+# remove("Hi!") === ""
+# remove("Hi! Hi!") === ""
+# remove("Hi! Hi! Hi!") === ""
+# remove("Hi Hi! Hi!") === "Hi"
+# remove("Hi! !Hi Hi!") === ""
+# remove("Hi! Hi!! Hi!") === "Hi!!"
+# remove("Hi! !Hi! Hi!") === "!Hi!"
+
+# def remove_word_with_one_em(s):
+#     pass
